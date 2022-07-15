909261e55d2b4436673ae7110ae980783045360e
swj369451 <swj369451@163.com>
2022-07-15 17:44:24 +0800

    更新

--- app/src/main/java/com/example/web_view/DownloadBlobFileJSInterface.java
+++ app/src/main/java/com/example/web_view/DownloadBlobFileJSInterface.java
@@ -11,28 +11,28 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DownloadBlobFileJSInterface {
-        private Context mContext;
-        private DownloadGifSuccessListener mDownloadGifSuccessListener;
+    private Context mContext;
+    private DownloadGifSuccessListener mDownloadGifSuccessListener;
 
-        public DownloadBlobFileJSInterface(Context context) {
-            this.mContext = context;
-        }
+    public DownloadBlobFileJSInterface(Context context) {
+        this.mContext = context;
+    }
 
-        public void setDownloadGifSuccessListener(DownloadGifSuccessListener listener) {
-            mDownloadGifSuccessListener = listener;
-        }
+    public void setDownloadGifSuccessListener(DownloadGifSuccessListener listener) {
+        mDownloadGifSuccessListener = listener;
+    }
 
-        @JavascriptInterface
-        public void getBase64FromBlobData(String base64Data,String fileName) {
-            convertToGifAndProcess(base64Data,fileName);
-        }
+    @JavascriptInterface
+    public void getBase64FromBlobData(String base64Data, String fileName) {
+        convertToGifAndProcess(base64Data, fileName);
+    }
 
-//        @JavascriptInterface
-//        public File getRecordList(){
-//            File file = new File(Environment.getExternalStoragePublicDirectory(
-//                    Environment.DIRECTORY_DOWNLOADS) + "/1.webm");
-//            return file;
-//        }
+//    @JavascriptInterface
+//    public File getRecordList(Object o) {
+//        File file = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_DOWNLOADS) + "/1.webm");
+//        return file;
+//    }
 
 //        /**
 //         * 插入js代码，转换成base64
@@ -60,39 +60,49 @@ public class DownloadBlobFileJSInterface {
 //            return "javascript: console.log('It is not a Blob URL');";
 //        }
 
-        /**
-         * 转换成file
-         * @param base64
-         */
-        private void convertToGifAndProcess(String base64,String fileName) {
-            File gifFile = new File(Environment.getExternalStoragePublicDirectory(
-                    Environment.DIRECTORY_DOWNLOADS) + "/" +fileName);
-            saveFileToPath(base64, gifFile);
-            if (mDownloadGifSuccessListener != null) {
-                mDownloadGifSuccessListener.downloadGifSuccess(gifFile.getAbsolutePath());
-            }
-        }
+    /**
+     * 转换成file
+     *
+     * @param base64
+     */
+    private void convertToGifAndProcess(String base64, String fileName) {
+        creatfile(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS));
+        File gifFile = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS) + "/" + fileName);
 
-
-        /**
-         * 保存文件
-         * @param base64
-         * @param gifFilePath
-         */
-        private void saveFileToPath(String base64, File gifFilePath) {
-            try {
-                byte[] fileBytes = Base64.decode(base64.replaceFirst(
-                        "data:image/gif;base64,", ""), 0);
-                FileOutputStream os = new FileOutputStream(gifFilePath, false);
-                os.write(fileBytes);
-                os.flush();
-                os.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        saveFileToPath(base64, gifFile);
+        if (mDownloadGifSuccessListener != null) {
+            mDownloadGifSuccessListener.downloadGifSuccess(gifFile.getAbsolutePath());
         }
+    }
 
-        public interface DownloadGifSuccessListener {
-            void downloadGifSuccess(String absolutePath);
+    private void creatfile(File dir) {
+        if (!dir.exists())
+            dir.mkdir();
+        return;
+    }
+
+    /**
+     * 保存文件
+     *
+     * @param base64
+     * @param gifFilePath
+     */
+    private void saveFileToPath(String base64, File gifFilePath) {
+        try {
+            byte[] fileBytes = Base64.decode(base64.replaceFirst(
+                    "data:image/gif;base64,", ""), 0);
+            FileOutputStream os = new FileOutputStream(gifFilePath, false);
+            os.write(fileBytes);
+            os.flush();
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } 
+    }
+
+    public interface DownloadGifSuccessListener {
+        void downloadGifSuccess(String absolutePath);
+    }
+}
--- app/src/main/java/com/example/web_view/MainActivity.java
+++ app/src/main/java/com/example/web_view/MainActivity.java
@@ -2,7 +2,10 @@
 package com.example.web_view;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,9 +22,20 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
+import org.webrtc.EglBase;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.ScreenCapturerAndroid;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+
+import java.net.URL;
 import java.util.HashMap;
 
 import androidx.annotation.NonNull;
@@ -37,12 +51,18 @@ public class MainActivity extends AppCompatActivity {
 
     //权限
     private final static int PERMISSIONS_REQUEST_CODE = 1;
+    private static final int REQ_REMOTE_HELP = 2;
     private final String[] permissions = {Manifest.permission.CAMERA,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
     private WebView mWebView;
+
+    private VideoCapturer videoCapturer;
+    private PeerConnectionFactory peerConnectionFactory;
+    private SurfaceTextureHelper surfaceTextureHelper;
+    private EglBase.Context eglBaseContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +72,68 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermission();
         }
+
+
+
+
+    }
+
+    private void initWebrtc() {
+        // create PeerConnectionFactory
+        PeerConnectionFactory.InitializationOptions initializationOptions =
+                PeerConnectionFactory.InitializationOptions.builder(this).createInitializationOptions();
+        PeerConnectionFactory.initialize(initializationOptions);
+         peerConnectionFactory = PeerConnectionFactory.builder().createPeerConnectionFactory();
+
+        eglBaseContext = EglBase.create().getEglBaseContext();
+         surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread", eglBaseContext);
+
+
+    }
+
+
+    private void screenShare() {
+        if (Build.VERSION.SDK_INT < 21) {
+            Toast.makeText(this, "您的手机不支持这个功能", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        MediaProjectionManager manager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        if (manager == null) {
+            Toast.makeText(this, "截屏服务不可用", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = manager.createScreenCaptureIntent();
+
+        startActivityForResult(intent, REQ_REMOTE_HELP);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_REMOTE_HELP && resultCode == RESULT_OK) {
+            VideoCapturer videoCapturer = new ScreenCapturerAndroid(data, new MediaProjection.Callback() {
+                @Override
+                public void onStop() {
+                    super.onStop();
+                }
+            });
+
+            VideoSource videoSource = peerConnectionFactory.createVideoSource(videoCapturer.isScreencast());
+            videoCapturer.initialize(surfaceTextureHelper, getApplicationContext(),videoSource.getCapturerObserver());
+            videoCapturer.startCapture(480, 640, 30);
+
+            SurfaceViewRenderer localView = findViewById(R.id.localView);
+            localView.setMirror(true);
+            localView.init(eglBaseContext, null);
+
+            // create VideoTrack
+            VideoTrack videoTrack = peerConnectionFactory.createVideoTrack("101", videoSource);
+            // display in localView
+            videoTrack.addSink(localView);
+        }
+
+
     }
 
     private void init() {
@@ -67,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                         //LogUtil.uploadH5Error(consoleMessage.message());
 //                        Toast.makeText(MainActivity.this, "webView==" + consoleMessage.message() + " level=" + consoleMessage.messageLevel(), Toast.LENGTH_SHORT).show();
 
-                           Log.e(TAG,"【webView==】" + consoleMessage.message());
+                        Log.e(TAG, "【webView==】" + consoleMessage.message());
 
 
                         break;
@@ -82,24 +164,24 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        mWebView.setWebViewClient(new WebViewClient(){
+        mWebView.setWebViewClient(new WebViewClient() {
             @Nullable
             @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-//                if(request.getUrl().toString().startsWith("http://localhost")){
-//                    File file = new File(Environment.getExternalStoragePublicDirectory(
-//                            Environment.DIRECTORY_DOWNLOADS) + "/1.webm");
-//                    FileInputStream fileInputStream=null;
+//
+//                if (request.getUrl().toString().contains("file:///storage/")) {
 //                    try {
-//                        fileInputStream = new FileInputStream(file);
+//                        File file = new File(Environment.getExternalStoragePublicDirectory(
+//                                Environment.DIRECTORY_DOWNLOADS) + "/1.webm");
+//                        return new WebResourceResponse("video/*", "UTF-8", new FileInputStream(file));
 //                    } catch (FileNotFoundException e) {
 //                        e.printStackTrace();
 //                    }
-//                    return new WebResourceResponse("video/webm", "vp9", fileInputStream);
 //                }
                 return super.shouldInterceptRequest(view, request);
             }
         });
+        mWebView.setWebContentsDebuggingEnabled(true);
 
 
         WebSettings webSettings = mWebView.getSettings();
@@ -107,13 +189,17 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAllowContentAccess(true);
+//        webSettings.setPluginState(WebSettings.PluginState.ON_DEMAND);
+//        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+//        webSettings.setAllowFileAccess(true);
+//        webSettings.setAllowFileAccessFromFileURLs(true);
+//        webSettings.setAllowContentAccess(true);
+//        webSettings.setAllowUniversalAccessFromFileURLs(true);
         //不使用缓存，只从网络获取数据.
         webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
 
         DownloadBlobFileJSInterface mDownloadBlobFileJSInterface = new DownloadBlobFileJSInterface(this);
-        mDownloadBlobFileJSInterface.setDownloadGifSuccessListener(absolutePath -> Toast.makeText(MainActivity.this, "下载成功，在Download目录下"+Environment.DIRECTORY_DOWNLOADS, Toast.LENGTH_LONG).show());
+        mDownloadBlobFileJSInterface.setDownloadGifSuccessListener(absolutePath -> Toast.makeText(MainActivity.this, "下载成功，在Download目录下" + Environment.DIRECTORY_DOWNLOADS, Toast.LENGTH_LONG).show());
 
 //        mWebView.setDownloadListener((url, userAgent, contentDisposition, mimeType, contentLength) -> {
 //            mWebView.loadUrl(DownloadBlobFileJSInterface.getBase64StringFromBlobUrl(url));
@@ -124,6 +210,11 @@ public class MainActivity extends AppCompatActivity {
 
         //js中使用java对象
         mWebView.addJavascriptInterface(mDownloadBlobFileJSInterface, "Android");
+
+//        //初始化屏幕
+//        screenShare();
+//        //初始化webrtc
+//        initWebrtc();
     }
 
 
